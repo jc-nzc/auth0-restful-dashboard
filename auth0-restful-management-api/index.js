6d089f4ffaa2a2fa58ce6102fc3341d1ca208e3d
JC <node.zerocool@gmail.com>
2021-01-24 21:56:28 -0600

    req

---
+++ auth0-restful-management-api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Auth0Manager = require('./Auth0Manager');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+require('dontenv').config();
+const apiPrefix = './api';
+
+const app = express();
+
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://${process.env.CLIENT_DOMAIN}/.well-known/jwks.json`
+    }),
+  });
